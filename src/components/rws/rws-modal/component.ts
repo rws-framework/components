438543b9d6b2ecd0ab462a125cad106b33f532bc
Papa Black <jakpawlik@gmail.com>
2025-07-07 11:20:07 +0200

    v0.1.0
    Changed files:
    - src/components/rws/rws-modal/component.ts
    - src/components/rws/rws-modal/styles/layout.scss
    - src/components/rws/rws-modal/template.html

--- src/components/rws/rws-modal/component.ts
+++ src/components/rws/rws-modal/component.ts
@@ -1,16 +1,29 @@
-import { observable } from '@microsoft/fast-element';
-import { RWSViewComponent, RWSView } from '@rws-framework/client';
+import { RWSViewComponent, RWSView, attr, observable } from '@rws-framework/client';
 
 @RWSView('rws-modal')
 class RWSModal extends RWSViewComponent {      
     @observable closeModal: () => void
     @observable onShowModal: ($: ShadowRoot) => void
 
+    @attr centerTop: 'true' | 'false' = 'true';
+    @attr centerLeft: 'true' | 'false' = 'true';
+
     connectedCallback(): void {
         super.connectedCallback(); 
         if(this.onShowModal){
             this.onShowModal(this.shadowRoot);
         }       
+    }
+
+    centerTopChanged(oldValue: 'true' | 'false', newValue: 'true' | 'false'): void {
+        this.centerTop = newValue;   
+        console.log('centerTop changed:', newValue);  
+    }
+
+    centerLeftChanged(oldValue: 'true' | 'false', newValue: 'true' | 'false'): void {
+        this.centerLeft = newValue; 
+        console.log('centerLeft changed:', newValue);  
+    
     }
 }
 
