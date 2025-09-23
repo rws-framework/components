90876d9eebc2d465e1263d49c725fbe70695e10a
Papa Black <jakpawlik@gmail.com>
2025-09-23 16:17:51 +0200

    v0.2.3
    Changed files:
    - package.json
    - src/components/rws/rws-modal/component.ts
    - src/components/rws/rws-modal/template.html

--- src/components/rws/rws-modal/component.ts
+++ src/components/rws/rws-modal/component.ts
@@ -2,17 +2,51 @@ import { RWSViewComponent, RWSView, attr, observable } from '@rws-framework/clie
 
 @RWSView('rws-modal')
 class RWSModal extends RWSViewComponent {      
-    @observable closeModal: () => void
-    @observable onShowModal: ($: ShadowRoot) => void
+    @observable closeModal?: () => void
+    @observable onShowModal?: ($: ShadowRoot) => void
 
     @attr centerTop: 'true' | 'false' = 'true';
     @attr centerLeft: 'true' | 'false' = 'true';
+    @attr({attribute: 'showCloseBtn', mode: 'boolean'}) showCloseBtn: boolean = true;
+
+    @attr name?: string;
 
     connectedCallback(): void {
         super.connectedCallback(); 
+        this.shadowRoot.ownerDocument.querySelector('body').classList.add('has-backdrop');
+        const ev = new CustomEvent(`rws_modal${this.name ? (':' + this.name) : ''}:open`, {
+            detail: true,
+            bubbles: true,
+            composed: true
+        });
+
+        this.dispatchEvent(ev);
+
         if(this.onShowModal){
             this.onShowModal(this.shadowRoot);
         }       
+
+        this.on('rws_modal:force_close', () => {
+            this.close();
+        })
+    }
+
+    close(){        
+        const ev = new CustomEvent(`rws_modal${this.name ? (':' + this.name) : ''}:close`, {
+            detail: true,
+            bubbles: true,
+            composed: true
+        });
+        this.dispatchEvent(ev);
+
+        if(this.closeModal){
+            this.closeModal();
+        }
+    }
+
+    disconnectedCallback(): void {
+        this.shadowRoot.ownerDocument.querySelector('body').classList.remove('has-backdrop');
+        super.disconnectedCallback();
     }
 
     centerTopChanged(oldValue: 'true' | 'false', newValue: 'true' | 'false'): void {
